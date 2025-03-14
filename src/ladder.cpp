48efe27fdc3bad7bffcc8376b9cce3c100ec74e6
Annie Yin <anniesy2@uci.edu>
2025-03-14 20:23:28 +0000

    wrote generate_word_ladder

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -2,6 +2,9 @@
 #include <string>
 #include <vector>
 #include <algorithm>
+#include <set>
+#include <queue>
+#include <set>
 
 void error(std::string word1, std::string word2, std::string msg) {
     std::cout << word1 << " " << word2 << msg << std::endl;
@@ -26,4 +29,24 @@ bool is_adjacent(const std::string& word1, const std::string& word2) {
     if (check.size() > 1)
         return false;
     return true;
+}
+
+std::vector<std::string> generate_word_ladder(const std::string& begin_word,
+const std::string& end_word, std::set<std::string>& word_list) {
+    std::queue<std::string> ladder_queue;
+    ladder_queue.push(begin_word);
+    std::set<std::string> visited;
+    visited.insert(begin_word);
+    for (std::string word : word_list) {
+        std::string last_word = ladder_queue.back();
+        if (is_adjacent(word, last_word)) {
+            if (visited.find(word) == visited.end()) {
+                visited.insert(word);
+                ladder_queue.push(word);
+            }
+        }
+        if (ladder_queue.back() == end_word)
+            break;
+    }
+    return ladder_queue;
 }
